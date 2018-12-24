17c0a70207828d187172c3b0c8b14bbcfde22a35
unknown <kumard2312@gmail.com>
2018-12-25 00:26:46 +0530

    v123 commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,22 +2,36 @@ import React, {Component} from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component{
     state={
         ingredients:{
-            salad:1,
-            bacon:1,
-            cheese:2,
-            meat:2
-        }
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        },
+        totalPrice:4
+    }
+    addIngredientHandler=(type)=>{
+      const oldCount=this.state.ingredients[type];
+      const updatedCount=oldCount+1;
+      const updatedIngredients={
+          ...this.state.ingredients
+      };
+      updatedIngredients[type]=updatedCount;
+
+    }
+    removeIngredientHandler=(type)=>{
+
     }
     render(){
         
         return(
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />
 
             </Auxiliary>
         );
