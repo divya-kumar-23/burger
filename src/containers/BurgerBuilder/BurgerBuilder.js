8314d25735dbf7c08e77592c0b7b42d8095dcad6
unknown <kumard2312@gmail.com>
2018-12-25 16:13:28 +0530

    v127 commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component{
             cheese:0,
             meat:0
         },
-        totalPrice:4
+        totalPrice:4,
+        purchasable:false
+    }
+    updatePurchaseState(ingredients){
+        
+        const sum=Object.keys(ingredients)
+        .map(igKey=>{
+            return ingredients[igKey]
+        })
+        .reduce((sum,el)=>{
+            return sum+el;
+        },0);
+        this.setState({purchasable:sum>0})
     }
     addIngredientHandler=(type)=>{
       const oldCount=this.state.ingredients[type];
@@ -34,6 +46,7 @@ class BurgerBuilder extends Component{
           totalPrice:newPrice,
           ingredients:updatedIngredients
       });
+      this.updatePurchaseState(updatedIngredients);
 
     }
     removeIngredientHandler=(type)=>{
@@ -53,6 +66,7 @@ class BurgerBuilder extends Component{
             totaltPrice:newPrice,
             ingredients:updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients);
     }
     render(){
         const disabledInfo={
@@ -68,6 +82,7 @@ class BurgerBuilder extends Component{
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
                 disabled={disabledInfo}
+                purchasable={this.state.purchasable}
                 price={this.state.totalPrice}
                 />
 
